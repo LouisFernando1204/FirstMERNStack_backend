9b9f3e2dadceb5b95bd7f999c610b1039a1d7764
Louis Fernando <ferlouis55@gmail.com>
2025-02-20 01:21:12 +0700

    Deploy to vercel

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,8 +1,5 @@
 const allowedOrigin = [
-    'http://localhost:3500',
-    'http://localhost:5173',
-    'https://www.dandrepairshop.com',
-    'http://dandrepairshop.com'
+    'https://technotes-frontend-alpha.vercel.app',
 ];
 
 module.exports = allowedOrigin;
